f02aedf50bedbfb9f325825231a795150f9417cf
shani sheladiya <rejoin.sunny13@gmail.com>
2020-02-06 01:36:18 +0530

    upload wallet version 2.0.2.4
    Add transaction list

--- src/assets/js/pages/form-wizard.init.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){$("#smartwizard-default").smartWizard({useURLhash:!1,showStepURLhash:!1}),$("#smartwizard-arrows").smartWizard({theme:"arrows",useURLhash:!1,showStepURLhash:!1}),$("#smartwizard-circles").smartWizard({theme:"circles",useURLhash:!1,showStepURLhash:!1}),$("#smartwizard-dots").smartWizard({theme:"dots",useURLhash:!1,showStepURLhash:!1})});
--- src/js/web3.js
+++
@@ -1,7 +0,0 @@
-// Create the main instance of web3 to be shared by everything in the app
-var Web3 = require('helios-web3');
-var web3 = new Web3();
-
-module.exports = {
-    web3: web3,
-};
